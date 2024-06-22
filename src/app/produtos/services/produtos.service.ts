a1fbd7284079f29b9bfcf6e470f996945d43b45c
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-06-22 09:49:42 -0300

    fix: :wrench: fixing angular.json assets and service

--- src/app/produtos/services/produtos.service.ts
+++ src/app/produtos/services/produtos.service.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import { Produto } from '../models/produto';
 
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutosService {
 
-  private readonly API = '../../../assets/produtos.json'
+  private readonly API = '/assets/produtos.json';
 
   constructor(public httpClient: HttpClient) { }
 
   list(){
-    return this.httpClient.get<Produto[]>(this.API);
+    return this.httpClient.get<Produto[]>(this.API).pipe(tap(produtos => console.log(produtos)));
   }
 }
