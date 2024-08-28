05c6103e89aae6b7c07b7937bb42b17564028b72
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-08-28 19:59:42 -0300

    feat: :seedling: taking data on form's to edit

--- src/app/produtos/produtos-form/produtos-form.component.ts
+++ src/app/produtos/produtos-form/produtos-form.component.ts
@@ -5,6 +5,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { AppMaterialModule } from '../../shared/app-material/app-material.module';
 import { ProdutosService } from '../services/produtos.service';
+import { ActivatedRoute } from '@angular/router';
+import { Produto } from '../models/produto';
 
 @Component({
   selector: 'app-produtos-form',
@@ -16,6 +18,7 @@ import { ProdutosService } from '../services/produtos.service';
 export default class ProdutosFormComponent {
 
   form = this.formBuilder.group({
+    _id :[0],
     nome: [''],
     descricao: [''],
     preco: [0],
@@ -23,26 +26,38 @@ export default class ProdutosFormComponent {
     categoria: ['']
   });
 
-constructor(
-  private formBuilder: NonNullableFormBuilder,
-  private service: ProdutosService,
-  private _snackBar: MatSnackBar,
-  private location: Location) {
-  //this.form;
-}
-onSubmit(){
-  console.log(this.form.value)
-  this.service.save(this.form.value).subscribe(data => this.onSucess(), error=>this.onError());
-  this.location.back();
-}
-onCancel(){
-  this.location.back();
-}
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private service: ProdutosService,
+    private _snackBar: MatSnackBar,
+    private location: Location,
+    private route: ActivatedRoute
+    ) {}
 
-private onError(){
-  this._snackBar.open("Erro ao salvar um novo produto, contate um programador.", '', {duration: 3000});
-}
-private onSucess(){
-  this._snackBar.open("Produto salvo com sucesso.", '', {duration: 3000});
-}
+  onSubmit(){
+    console.log(this.form.value)
+    this.service.save(this.form.value).subscribe(data => this.onSucess(), error=>this.onError());
+    this.location.back();
+  }
+  onCancel(){
+    this.location.back();
+  }
+
+  private onError(){
+    this._snackBar.open("Erro ao salvar um novo produto, contate um programador.", '', {duration: 3000});
+  }
+  private onSucess(){
+    this._snackBar.open("Produto salvo com sucesso.", '', {duration: 3000});
+  }
+  ngOnInit(): void{
+    const produto: Produto = this.route.snapshot.data['produto'];
+    this.form.setValue({
+      _id: produto._id,
+      nome: produto.nome,
+      descricao: produto.descricao,
+      preco: produto.preco,
+      imagem: produto.imagem,
+      categoria: produto.categoria
+    });
+  }
 }
